a2b4e54a1a5f2837822bcacf9d4c6af232e6724d
Toan Vu Duc <toanvd@vega.com.vn>
2022-04-04 16:21:19 +0700

    Update login page of cms

--- app/Http/Controllers/cms/Home.php
+++ app/Http/Controllers/cms/Home.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 
 class Home extends Controller
 {
+    public function login(){
+        return view('cms.layouts.login');
+    }
+
     public function index()
     {
         return view('cms.home.index');
--- routes/cms.php
+++ routes/cms.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\cms\Home::class,'index'])->name('home');
+Route::get('/', [\App\Http\Controllers\cms\Home::class, 'index'])->name('home');
+
+Route::get('/login', [\App\Http\Controllers\cms\Home::class, 'login'])->name('login');

7113ff8ac67e9002f2edd44499894e4fba95be16
AutoTool-Game <toanvu2000dq@gmail.com>
2022-04-23 12:51:39 +0700

    Update cms

--- routes/cms.php
+++ routes/cms.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\cms\Home;
+use \App\Http\Controllers\cms\Auth\Auth;
+use \App\Http\Controllers\cms\Video;
 
-Route::get('', [\App\Http\Controllers\cms\Home::class, 'index'])->name('cms.home');
+Route::get('', [Home::class, 'index'])->name('cms.home');
 
-Route::get('login', [\App\Http\Controllers\cms\Auth\Auth::class, 'getLogin'])->name('login');
-Route::get('logout', [\App\Http\Controllers\cms\Auth\Auth::class, 'getLogout'])->name('logout');
-Route::post('login',[\App\Http\Controllers\cms\Auth\Auth::class, 'postLogin']);
+Route::get('login', [Auth::class, 'getLogin'])->name('cms.login');
+Route::get('logout', [Auth::class, 'getLogout'])->name('cms.logout');
+Route::post('login',[Auth::class, 'postLogin']);
+
+Route::get('video', [Video::class, 'index'])->name('cms.video');
